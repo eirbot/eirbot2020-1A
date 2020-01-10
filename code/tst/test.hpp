1e49993fccfe4ba091a11583b38fb9d9fe274cf4
sedelpeuch <Sebastien.Delpeuch@bordeaux-inp.fr>
2020-01-10 18:42:18 +0100

    Mise en place des tests

---
+++ code/tst/test.hpp
@@ -0,0 +1,22 @@
+#ifndef __TEST_H_
+#define __TEST_H_
+
+#include <vector>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <array>
+#include <cfloat>
+
+#include "../src/navigation.h"
+#include "../src/asserv.hpp"
+#include "../src/detection.hpp"
+#include "../src/actionneur.hpp"
+
+
+int main(int argc, char *argv[]);
+
+
+
+
+#endif // __TEST_H_

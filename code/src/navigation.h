214492ef56f556a6dfcf9886eba30b4a6be1139c
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-01-11 12:16:54 +0100

    class navig : dimension correctes + nav->asserv

--- code/src/navigation.h
+++ code/src/navigation.h
@@ -7,11 +7,13 @@
 #include <math.h>
 #include <array>
 #include <cfloat>
+#include <stack>
+
+#include "asserv.hpp"
 
 #define X_MAX 300 //Longueur de la table
 #define Y_MAX 200 //Largeur de la table
 
-static int test=0;
 
 struct Node{
         ushort x;
@@ -35,8 +37,10 @@ class Navigation
                 static bool isDestination(int x, int y, Node dest); //Permet de savoir si le noeud est la destination ou non
                 static double calculateE(int x, int y, Node dest);//Permet de calculer la distance euclidienne entre deux noeuds
                 static std::vector<Node> Astar(Node src, Node dest); //Coeur de l'algorithme A*
-                static std::vector<Node> constructPath(Node src, Node dest); //Permet de recréer le chemin
-                static bool onTable(Node pos);
+                static std::vector<Node> MakePath(std::array<std::array<Node,Y_MAX>,X_MAX> map, Node dest); //Permet de recréer le chemin
+                static bool onTable(Node pos); //Permet de savoir si on est sur la table
+                static void Print_path(std::vector<Node> usablePath); //Juste du débeugage permet d'afficher coordonnées par coordonnées un chemin
+                static void Navigate_to_asserv(std::vector<Node>usablePath); //Permet de convertir un chemin en instruction asserv
                 ~Navigation();
 
 //Attributs

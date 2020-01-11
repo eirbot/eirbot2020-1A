18cbe4af80a8b0b17179c8fc1552881555b8a7f4
sedelpeuch <Sebastien.Delpeuch@bordeaux-inp.fr>
2020-01-11 12:17:50 +0100

    Tests à la main du A*

--- code/tst/test.cpp
+++ code/tst/test.cpp
@@ -1,6 +1,6 @@
 #include "test.hpp"
 int main(int argc, char *argv[]) {
-    printf("Test en cours veuillez patienter");
+    printf("Test en cours veuillez patienter \n");
     Node src;
     src.x=0;
     src.y=0;
@@ -8,15 +8,15 @@ int main(int argc, char *argv[]) {
     dest.x=0;
     dest.y=0;
     std::vector<Node> result;
-    for (int i=0; i < 300; i++) {
-        dest.x+=1;
+    for (int i=0; i < 299; i++) {
+        dest.x=i;
         printf(" .");
-        for (int j=0; j < 200; j++) {
-            dest.y+=1;
+        for (int j=0; j < 199; j++) {
+            dest.y=j;
+            printf("%d %d \n",i, j);
             result=Navigation::Astar(src,dest);
         }
     }
     printf("\n");
-    printf("%d \n",test);
     return 0;
 }

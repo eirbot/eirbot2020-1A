b0e77cfb38841a7ddb6327f667b965a859178fc1
sedelpeuch <Sebastien.Delpeuch@bordeaux-inp.fr>
2020-01-12 13:29:13 +0100

    Nouveau test

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -5,13 +5,16 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     Node src;
-    src.x=0;
-    src.y=0;
+    src.x=50;
+    src.y=50;
     Node dest;
     dest.x=atoi(argv[1]);
     dest.y=atoi(argv[2]);
+    vector <obstacle> list_obstacles=World::fillVector();
     std::vector<Node> result;
-    result=Navigation::Astar(src,dest);
-    Navigation::Navigate_to_asserv(result);
+    result=Navigation::Astar(src,dest,list_obstacles);
+    if(result.size()!=0){
+        Navigation::Navigate_to_asserv(result);
+    }
     return 0;
 }

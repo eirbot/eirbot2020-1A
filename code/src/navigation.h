519145b779d584a3f197841fe71991aeb4e1445e
sedelpeuch <Sebastien.Delpeuch@bordeaux-inp.fr>
2020-01-12 13:29:23 +0100

    Modification astar

--- code/src/navigation.h
+++ code/src/navigation.h
@@ -9,23 +9,12 @@
 #include <cfloat>
 #include <stack>
 
+#include "world.hpp"
 #include "asserv.hpp"
+#include "main.hpp"
 
 #define X_MAX 300 //Longueur de la table
 #define Y_MAX 200 //Largeur de la table
-
-
-struct Node{
-        ushort x;
-        ushort y;
-        ushort parentX;
-        ushort parentY;
-        float gcost;
-        float hcost;
-        float fcost;
-
-};
-
 
 class Navigation
 {
@@ -33,10 +22,9 @@ class Navigation
         public:
                 Navigation();
                 Navigation(Node src, Node dest);
-                static bool isValid(int x, int y); //Permet de savoir si le noeud est obstacle ou non
                 static bool isDestination(int x, int y, Node dest); //Permet de savoir si le noeud est la destination ou non
                 static double calculateE(int x, int y, Node dest);//Permet de calculer la distance euclidienne entre deux noeuds
-                static std::vector<Node> Astar(Node src, Node dest); //Coeur de l'algorithme A*
+                static std::vector<Node> Astar(Node src, Node dest, std::vector<obstacle> list_obstacles); //Coeur de l'algorithme A*
                 static std::vector<Node> MakePath(std::array<std::array<Node,Y_MAX>,X_MAX> map, Node dest); //Permet de recréer le chemin
                 static bool onTable(Node pos); //Permet de savoir si on est sur la table
                 static void Print_path(std::vector<Node> usablePath); //Juste du débeugage permet d'afficher coordonnées par coordonnées un chemin

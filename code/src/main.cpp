df333bacca7bca9246a8df0529d8e393c22d4ae6
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-01-12 15:33:38 +0100

    Adaptation classe navigation (pour être utilisée)

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -2,19 +2,16 @@
 #include <iostream>
 
 using namespace std;
+class Navigation;
+class World;
 
 int main(int argc, char *argv[]) {
-    Node src;
-    src.x=50;
-    src.y=50;
-    Node dest;
-    dest.x=atoi(argv[1]);
-    dest.y=atoi(argv[2]);
-    vector <obstacle> list_obstacles=World::fillVector();
-    std::vector<Node> result;
-    result=Navigation::Astar(src,dest,list_obstacles);
-    if(result.size()!=0){
-        Navigation::Navigate_to_asserv(result);
+  Navigation src({50, 50,0,0,0,0,0}), dest({(short) atoi(argv[1]), (short) atoi(argv[2]),0,0,0,0,0});
+  vector<obstacle> list_obstacles = World::fillVector();
+  std::vector<Node> result;
+  result = Navigation::Astar(src, dest, list_obstacles);
+  if (result.size() != 0) {
+    Navigation::Navigate_to_asserv(result);
     }
     return 0;
 }

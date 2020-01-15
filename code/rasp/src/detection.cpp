6767ae565a8834781780cf1900ed9a85b8b562da
sedelpeuch <Sebastien.Delpeuch@bordeaux-inp.fr>
2020-01-15 08:10:57 +0100

    Modification mineure du makefile

---
+++ code/rasp/src/detection.cpp
@@ -0,0 +1,18 @@
+#include "detection.hpp"
+
+GP2::GP2():input(0),activated(0),distance_seuil(50)
+{
+
+}
+
+GP2::GP2(int input,int activated,int distance_seuil):input(input),activated(activated),distance_seuil(distance_seuil)
+{
+
+}
+
+
+
+GP2::~GP2()
+{
+ 
+}

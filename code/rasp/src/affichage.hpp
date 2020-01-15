566bb317845e1da1a7ae306a0f4d7ca704d67a73
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-01-15 08:09:59 +0100

    Création d'affichage pour le debug

---
+++ code/rasp/src/affichage.hpp
@@ -0,0 +1,24 @@
+#ifndef __AFFICHAGE_H_
+#define __AFFICHAGE_H_
+
+#include <cstdio>
+
+#ifndef TIMEOUT
+#define TIMEOUT 0
+#endif
+
+extern bool debug;
+
+void print_timeout();
+
+void print_success();
+
+void print_fail();
+
+void affichage(int timeout);
+
+void good_port(short result_x, short result_y, short dest_x, short dest_y);
+
+void print_optimisated();
+
+#endif // __AFFICHAGE_H_

8d21d85ab3f0464c4d2a8e413b78d0de814e6277
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-01-19 16:14:30 +0100

    Ajout de la fonctionne de détection (attente de mise en place de test)

--- code/rasp/src/detection.cpp
+++ code/rasp/src/detection.cpp
@@ -26,8 +26,11 @@ extern vector<obstacle> list_obstacles;
 
 static void gp2Obstacle()
 {
+    //On récupère la valeur de la nucléo
     int nucleo_value[6]={0,0,0,0,0,0}; //set:detection() pour récupérer l'information selon le protocole de com
     int tot=0;
+
+    // On vérifie si il y a des informations
     for (int i=0; i < 6; i++) {
         if (nucleo_value[i]==0) {
             tot+=1; //Pas d'interuption
@@ -37,39 +40,57 @@ static void gp2Obstacle()
         return;
     }
 
-    struct GP2_information information_avant[3];//nucleo_value normalement;
+    //Si il y a des informations on transforme ces dernières et on les place des deux tableaux (avant et arrière)
+    struct GP2_information information_avant[3];
     struct GP2_information information_arriere[3];
     for (int i=0; i < 3; i++) {
-        information_avant[i].mask_4=(nucleo_value[i]%2);
+        information_avant[i].mask=(nucleo_value[i]%2);
         nucleo_value[i]=nucleo_value[i]/2;
-        information_avant[i].mask_3=(nucleo_value[i]%2);
+        information_avant[i].mask+=(nucleo_value[i]%2);
         nucleo_value[i]=nucleo_value[i]/2;
-        information_avant[i].mask_2=(nucleo_value[i]%2);
+        information_avant[i].mask+=(nucleo_value[i]%2);
         nucleo_value[i]=nucleo_value[i]/2;
-        information_avant[i].mask_1=(nucleo_value[i]%2);
+        information_avant[i].mask+=(nucleo_value[i]%2);
         nucleo_value[i]=nucleo_value[i]/2;
-        information_avant[i].input=(enum GP2_name) nucleo_value[i];
+        information_avant[i].input=(enum GP2_name) i;
 
-        information_arriere[i].mask_4=(nucleo_value[i+3]%2);
+        information_arriere[i].mask=(nucleo_value[i+3]%2);
         nucleo_value[i]=nucleo_value[i+3]/2;
-        information_arriere[i].mask_3=(nucleo_value[i+3]%2);
+        information_arriere[i].mask+=(nucleo_value[i+3]%2);
         nucleo_value[i]=nucleo_value[i+3]/2;
-        information_arriere[i].mask_2=(nucleo_value[i+3]%2);
+        information_arriere[i].mask+=(nucleo_value[i+3]%2);
         nucleo_value[i]=nucleo_value[i+3]/2;
-        information_arriere[i].mask_1=(nucleo_value[i+3]%2);
+        information_arriere[i].mask+=(nucleo_value[i+3]%2);
         nucleo_value[i]=nucleo_value[i+3]/2;
-        information_arriere[i].input=(enum GP2_name) nucleo_value[i+3];
+        information_arriere[i].input=(enum GP2_name) (i+3);
     }
-    int max_mask_avant=0;
-    int max_mask_arriere=0;
+
+    //On trouve le mask le plus proche de nous
+    // 4 -> 6 cm
+    // 3 -> 10 cm
+    // 2 -> 20 cm
+    // 1 -> 50 cm
+    // 0 -> rien
+    short max_mask_avant=0;
+    short max_mask_arriere=0;
     for (int i=0; i < 3; i++) {
-        max_mask_avant=4-information_avant[i].mask_1-information_avant[i].mask_2-information_avant[i].mask_3-information_avant[i].mask_4;
-        max_mask_arriere=4-information_arriere[i].mask_1-information_arriere[i].mask_2-information_arriere[i].mask_3-information_arriere[i].mask_4;
+        if(max_mask_avant<information_avant[i].mask){
+            max_mask_avant=information_avant[i].mask;
+        }
+        if (max_mask_arriere < information_arriere[i].mask){
+            max_mask_arriere = information_arriere[i].mask;
+        }
     }
 
-    list_obstacles.push_back(value_type &&__x)
+    //On trouve maintenant la configuration (gauche, centre gauche, centre, centre droit, droit) et on place le nouvel obstacle
+    short position_x=Asservissement::robot_position().x;
+    short position_y=Asservissement::robot_position().y;
+    short angle=Asservissement::angle();
+    struct shape robot={34,34};
+    float calcul_x=position_x+(max_mask_avant+32)*cos(angle);
+    float calcul_y=position_y+(max_mask_avant+32)*sin(angle);
+    list_obstacles.push_back({(short)calcul_x, (short)calcul_y, &robot});
 }
-
 
 //Creer une interuption si les GP2 activés détectent quelque chose
 

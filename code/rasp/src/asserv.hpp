e7f56da7c3b3dbfea7833d320b9dc5460c6c4048
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-01-19 16:13:35 +0100

    Ajout fonction angle et supression de backword et forward

--- code/rasp/src/asserv.hpp
+++ code/rasp/src/asserv.hpp
@@ -30,8 +30,7 @@ class Asservissement
         static void go_to(struct position dest); //Permet au robot de se tourner vers la destination et d'aller à la destination
         static void rotate(short angle); //Permet de faire tourner le robot d'un certain angle (le type de angle est à définir)
         static struct position robot_position(); //Renvoie la position du robot
-        static void backward(); //Met les moteurs en marche arrière
-        static void forward(); //Met les moteurs en marche avant
+        static short angle(); //Renvoie l'angle du robot par rapport à la table (entre 0 et 360)
         static void initialise_x(); //Fait la calibration par rapport au grand coté de la table
         static void initialise_y(); //Fait la calibration par rapport au petit coté de la table
     private:

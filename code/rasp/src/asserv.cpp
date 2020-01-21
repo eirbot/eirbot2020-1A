f0041a03bcaadb3a470e96ffcad6a092ae00bf64
sedelpeuch <Sebastien.Delpeuch@bordeaux-inp.fr>
2020-01-21 22:14:46 +0100

    Modification du type de retour de call_back, renvoie l'état de la requette (0 = ok , 1 = timeout , 2 = detection) Ajout de la fonction call_back pour calculer ça de manière propre

--- code/rasp/src/asserv.cpp
+++ code/rasp/src/asserv.cpp
@@ -2,8 +2,12 @@
 
 //Les printfs seront à modifier pour envoyer directement au protocol de comm.
 
-void Asservissement::go_to(struct position dest)
+extern vector<obstacle> list_obstacles;
+int Asservissement::go_to(struct position dest)
 {
+    if(dest.x==39 && dest.y==58){
+        return 2;
+    }
     if(debug==1){
         debugPath.push_back({.x=(short) dest.x,.y= (short) dest.y,0,0,0,0,0});
     }
@@ -14,38 +18,49 @@ void Asservissement::go_to(struct position dest)
             print_optimisated();
         }
     }
-    affichage(TIMEOUT);
+    affichage(TIMEOUT,DETECTION);
+    return Asservissement::call_back(TIMEOUT,DETECTION);
 }
 
+int Asservissement::call_back(int timeout, int detection)
+{
+    if(timeout==1){
+        return 1;
+    }
+    if(detection==1){
+        return 2;
+    }
+    return 0;
+}
 
 void Asservissement::rotate(short angle)
 {
     printf("Envoie de la requete de rotation de theta:%3d ............ ",angle);
-    affichage(TIMEOUT);
+    affichage(TIMEOUT,DETECTION);
 }
 
 struct position Asservissement::robot_position()
 {
     printf("Envoie de la requete d'information sur la position ....... ");
-    affichage(TIMEOUT);
+    affichage(TIMEOUT,DETECTION);
     return {.x=16,.y=80};
 }
 
 short Asservissement::angle()
 {
     printf("Envoie de la requete d'information sur l'angle ........... ");
-    affichage(TIMEOUT);
+    affichage(TIMEOUT,DETECTION);
     return 0; //Pour l'instant
 }
 
 void Asservissement::initialise_x()
 {
     printf("Envoie de la requete d'initialisation en x ............... ");
-    affichage(TIMEOUT);
+    affichage(TIMEOUT,DETECTION);
 }
 
 void Asservissement::initialise_y()
 {
     printf("Envoie de la requete d'initialisation en y ............... ");
-    affichage(TIMEOUT);
+    affichage(TIMEOUT,DETECTION);
 }

8f00a04fdaaecdea62d51d016701b1fb87fc7c8f
lucas <lucas-m3>
2020-01-23 12:59:11 +0100

    suite protocole reception / emission nucleo

--- code/rasp/tst/test_protocole.cpp
+++ code/rasp/tst/test_protocole.cpp
@@ -4,14 +4,32 @@
 int main(int argc, char *argv[]) {
     if(argc > 1) {
         Protocole proto(argv[1]);
-        proto.set_position(10, 10);
+
+        usleep(500000); //0.5s
+
         proto.get_position();
-        proto.set_angle(90);
-        proto.get_angle();
-        proto.get_etats_GP2();
-        proto.set_seuils_GP2('0','0',30);
-        //sleep(5);
+        usleep(500000); //0.5s
         proto.update_buffer();
+
+/*        proto.set_position(10, 10);
+        usleep(500000); //0.5s
+        proto.update_buffer();
+
+        proto.set_angle(90);
+        usleep(500000); //0.5s
+        proto.update_buffer();
+
+        proto.get_angle();
+        usleep(500000); //0.5s
+        proto.update_buffer();
+
+        proto.get_etats_GP2();
+        usleep(500000); //0.5s
+        proto.update_buffer();
+
+        proto.set_seuils_GP2('0','0',30);
+        usleep(500000); //0.5s
+        proto.update_buffer();*/
     }
     else {
         printf("Usage : ./test_protocole [port serie]\n");

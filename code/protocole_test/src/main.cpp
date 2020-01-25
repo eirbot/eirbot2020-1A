f2a2e4b995bed019a0388a2dc08b202862a212fa
lucas <lucas-m3>
2020-01-25 16:20:30 +0100

    les bons fichiers c'est mieux

--- code/protocole_test/src/main.cpp
+++ code/protocole_test/src/main.cpp
@@ -1,14 +1,16 @@
 #include "mbed.h"
-#include "platform/ATCmdParser.h"
-#include "drivers/Serial.h"
+// #include "drivers/Serial.h"
 
 
+DigitalIn button(USER_BUTTON);
 DigitalOut info_led(LED1);
 DigitalOut debug_led(LED2);
-Serial *_serial;
-ATCmdParser *_parser;
+DigitalOut data_led(LED3);
+RawSerial _serial(USBTX, USBRX);
 short x, y, angle, dist;
 char id, palier;
+char readBuffer[64];
+int buf_index = 0;
 
 void blink(DigitalOut led, unsigned char n, float delay) {
     for (unsigned char i = 0; i < n; i++) {
@@ -19,6 +21,18 @@ void blink(DigitalOut led, unsigned char n, float delay) {
     }
 }
 
+void parse() {
+    data_led = 1;
+    //SET
+    if(sscanf(readBuffer, "SPO%hd,%hd\n", &x, &y)) {
+        _serial.printf("RPOOK\n");
+    }
+    else if(sscanf(readBuffer, "SRO%hd\n", &angle)) {
+        _serial.printf("RROOK\n");
+    }
+    data_led = 0;
+}
+
 /*
  * S = Set
  * G = Get
@@ -26,49 +40,29 @@ void blink(DigitalOut led, unsigned char n, float delay) {
  * R = Reponse : RPOOK, RPOOUT,
  */
 void poll() {
-    if(_parser->recv("GPO")) {
-        _parser->send("VPO%hd, %hd", x, y);
-        return;
-    }
-    blink(debug_led, 3, 0.05f);
-    if(_parser->recv("SPO%hd,%hd", &x, &y)) { //13*8bits = 104 octets -> Ttrans = 104/115200 = 1 ms
-        _parser->send("RPOOK");
-        return;
-    }
-    if(_parser->recv("SRO%hd", &angle)) {
-        _parser->send("RROOK");
-        return;
-    }
-    if(_parser->recv("GRO")) {
-        _parser->send("RRO%hd", angle);
-        return;
-    }
-    if(_parser->recv("GGE")) {
-        _parser->send("VGE0,1,2,3");
-        return;
-    }
-    if(_parser->recv("SGS%c,%c,%hd", &id, &palier, &dist)) {
-        _parser->send("RGSOK");
-        return;
+    while(_serial.readable()) {
+        readBuffer[buf_index] = _serial.getc();
+        if(readBuffer[buf_index] == '\n') {
+            readBuffer[buf_index+1] = 0;
+            buf_index = 0;
+            parse();
+            break;
+        }
+        buf_index++;
     }
 }
 
 int main(int argc, char *argv[]) {
-    _serial = new Serial(USBTX, USBRX);
-    _serial->baud(115200);
-    _parser = new ATCmdParser(_serial);
-    _parser->set_delimiter("\n");
-    _parser->set_timeout(500);
-    _parser->debug_on(1);
-
-    //_serial->attach(&poll, Serial::RxIrq);
-
-//    _parser->oob("SPO%hd,%hd\n", set_pos);
-//    _parser->process_oob();
+    _serial.baud(115200);
+    _serial.attach(&poll, Serial::RxIrq);
 
     while(1) {
         blink(info_led, 10, 0.1f);
-        poll();
+        if(button.read()) {
+            blink(data_led, 5, 0.05f);
+            _serial.printf("x: %hd, y: %hd\n", x, y);
+            _serial.printf("angle: %hd\n", angle);
+        }
     }
 
 

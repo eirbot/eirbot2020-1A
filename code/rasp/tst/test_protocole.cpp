9cc1ab8684cbbb3e772261d91b3c105bf830e5f8
lucas <lucas-m3>
2020-01-29 22:13:41 +0100

    debut code timeout

--- code/rasp/tst/test_protocole.cpp
+++ code/rasp/tst/test_protocole.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    char etats[3];
     if(argc > 1) {
         Protocole proto(argv[1]);
 
@@ -9,34 +10,34 @@ int main(int argc, char *argv[]) {
 
         printf("Set GP2 on\n");
         proto.set_detection_GP2(1);
-        usleep(100000); //0.1s
+        //usleep(100000); //0.1s
         printf("=================================\n");
 
         printf("Set Position\n");
-        proto.set_position(10, 10);
-        usleep(100000); //0.1s
+        proto.set_position(10, 10, etats);
+        //usleep(100000); //0.1s
         printf("ANGLE:\n");
         printf("=================================\n");
 
         printf("Set angle\n");
         proto.set_angle(90);
-        usleep(100000); //0.1s
+        //usleep(100000); //0.1s
         printf("=================================\n");
 
         printf("Get Position\n");
         proto.get_position();
-        usleep(100000); //0.1s
+        //usleep(100000); //0.1s
         printf("=================================\n");
 
         printf("Get angle\n");
         proto.get_angle();
-        usleep(100000); //0.1s
+        //usleep(100000); //0.1s
         printf("=================================\n");
 
         printf("Get etats\n");
         char etats_GP2[3];
         proto.get_etats_GP2(etats_GP2);
-        usleep(100000); //0.1s
+        //usleep(100000); //0.1s
         printf("=================================\n");
     }
     else {

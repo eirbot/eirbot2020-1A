cebb8257b09384e8f64253de24dfb62bbce714b6
sedelpeuch <Sebastien.Delpeuch@bordeaux-inp.fr>
2020-02-01 21:36:17 +0100

    Début protocole

--- code/rasp/src/asserv.hpp
+++ code/rasp/src/asserv.hpp
@@ -12,23 +12,26 @@
 
 #include  "affichage.hpp"
 #include "world.hpp"
+#include "protocole.hpp"
 #include <vector>
 
 using namespace std;
 
 extern bool debug;
 extern vector<Node> debugPath;
+extern class Protocole Protocole;
 
 struct position{
     int x;
     int y;
 };
 
+
 class Asservissement
 {
     public:
         static int go_to(struct position dest); //Permet au robot de se tourner vers la destination et d'aller à la destination
-        static int call_back(int timeout, int detection); //Permet d'analyser le retour de l'asservissement
+        static int call_back(enum Protocole::Etat asserv_back); //Permet d'analyser le retour de l'asservissement
         static void rotate(short angle); //Permet de faire tourner le robot d'un certain angle (le type de angle est à définir)
         static struct position robot_position(); //Renvoie la position du robot
         static short angle(); //Renvoie l'angle du robot par rapport à la table (entre 0 et 360)

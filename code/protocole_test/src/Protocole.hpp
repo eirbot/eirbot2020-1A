8cc409e88ef2cce601ae50b6daab5485357d50d0
lucas <lucas-m3>
2020-02-01 15:32:53 +0100

    creation de la classe protocole
    classe protocole fonctionnelle, s'inspirer du main.cpp pour l'utilisation

---
+++ code/protocole_test/src/Protocole.hpp
@@ -0,0 +1,26 @@
+#ifndef __PROTOCOLE_H_
+#define __PROTOCOLE_H_
+
+#include "mbed.h"
+
+class Protocole
+{
+    public:
+        Protocole();
+        ~Protocole();
+
+        short x, y, angle;
+        char GP2_on;
+        char GP2_etats[3];
+
+        void poll();
+
+    private:
+        char readBuffer[32];
+        int buf_index = 0;
+        RawSerial *_serial;
+        void parse();
+};
+
+
+#endif // __PROTOCOLE_H_

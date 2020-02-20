6e4a37a79842a70b99142a4adc2e024090ce9abb
lucas <lucas-m3>
2020-02-20 17:50:48 +0100

    nucleo actionneur

--- code/nucleo/src/main.cpp
+++ code/nucleo/src/main.cpp
@@ -1,8 +1,28 @@
 #include "mbed.h"
+#include <Protocole.hpp>
 
+DigitalOut LD1_led(LED1);
+DigitalOut LD2_led(LED2);
+DigitalOut LD3_led(LED3);
+Protocole protocole;
+
+void blink(DigitalOut led, unsigned char n, float delay) {
+    for (unsigned char i = 0; i < n; i++) {
+        led= 1;
+        wait(delay);
+        led = 0;
+        wait(delay);
+    }
+}
 
 int main(int argc, char *argv[]) {
 
+    protocole.GP2_etats[0] = '1';
+    protocole.GP2_etats[1] = '0';
+    protocole.GP2_etats[2] = '1';
 
+    while(1) {
+        blink(LD1_led, 10, 0.1f); //simulation d'activité
+    }
     return 0;
 }

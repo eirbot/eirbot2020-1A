f90499d27ec96a194558ec98f365a7a8b63e4a9c
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-02-21 21:01:08 +0100

    maj dossier présentation

--- code/rasp/src/actionneur.hpp
+++ code/rasp/src/actionneur.hpp
@@ -4,14 +4,14 @@
 #include "world.hpp"
 #include "asserv.hpp"
 #include "navigation.hpp"
+#include "protocole.hpp"
 
 class Actionneur{
 
     public:
         static void Phare_activation(); //Sort l'actionneur et avance un peu pour activer le phare
         static void Phare_desactivation(); //Rentre l'actionneur
-        static void Manche_activation(); //Sort l'actioneur
-        static void Manche_desactivation(); //Rentre l'actionneur
+        static void Pavillon(); //Sort l'actioneur
     private:
 };
 

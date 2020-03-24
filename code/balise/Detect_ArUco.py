a33921f160bfa3e39a93df17eb2f771e25ecc1ed
Starmaxou <56699804+Starmaxou@users.noreply.github.com>
2020-03-24 17:08:09 +0100

    Ajout 1ère version Code Balise
    Ajout de la 1ère version du code de la balise. 
    Code non optimisé pour le moment et ne discrimine pas le tag 17.
    Ne prends pas encore en compte le module sans fil.

---
+++ code/balise/Detect_ArUco.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+ 
+cap = cv2.VideoCapture(0)
+ 
+while(True):
+    #Lecture d'une image 
+    ret, frame = cap.read()
+
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+    parameters =  aruco.DetectorParameters_create()
+
+    #Detection des tags
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+
+    #TODO
+    #Fonction de descrimination des tag autre que le tag 17
+
+    #Calcul de l'orientation du tag
+    if corners :
+        haut = (corners[0][0][0][1] + corners[0][0][1][1])/2
+        bas = (corners[0][0][2][1] + corners[0][0][3][1])/2
+
+        if (haut < bas):
+            print('Nord')
+        else :
+            print('Sud')
+    else :
+        print('no ArUco detected')
+
+    #Affichage des tag sur l'image de la caméra
+    # /!\ DESACTIVER SUR VERSION FINAL /!\
+    out = aruco.drawDetectedMarkers(frame, corners, ids)
+    cv2.imshow('frame_r',out)
+    #/!\ /!\
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        break
+ 
+# Fermeture de la webcam et fermeture des fenêtres
+cv2.destroyAllWindows()

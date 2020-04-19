b66eebbe51522bd9021acbb3ccb6785aa0173670
DELPEUCH Sébastien <sedelpeuch@enserib-matmeca.fr>
2020-04-19 17:04:42 +0200

    Documentation : Ajout de la documentation du dossier rasp/

--- code/rasp/src/actionneur.hpp
+++ code/rasp/src/actionneur.hpp
@@ -7,13 +7,32 @@
 #include "protocole.hpp"
 #include "affichage.hpp"
 
+/**
+ * @file actionneur.hpp
+ * @brief Ce fichier contient la classe permettant de gérer les actionneurs
+ * @author SD
+ */
+
+/**
+ * @brief Contient toutes les méthodes permettant de gérer les actionneurs du robot */
 class Actionneur{
 
     public:
+        /**
+        * @brief Permet de demander au protocole la sortie de l'actionneur
+        * @return Rien, à modifier pour la compatibilité avec le protocole
+         */
         static void Phare_activation(); //Sort l'actionneur et avance un peu pour activer le phare
+        /**
+         * @brief Permet de demander au protocole la rentrée de l'actionneur
+         * @return Rien, à modifier pour la compatibilité avec le protocole
+         */
         static void Phare_desactivation(); //Rentre l'actionneur
+        /**
+         * @brief Permet de demander au protocole l'activation du pavillon
+         * @return Rien, à modifier pour la compatibilité avec le protocole
+        */
         static void Pavillon(); //Sort l'actioneur
-    private:
 };
 
 #endif
--- code/rasp/src/affichage.hpp
+++ code/rasp/src/affichage.hpp
@@ -7,6 +7,12 @@
 #include "navigation.hpp"
 #include "protocole.hpp"
 
+/**
+ * @file affichage.hpp
+ * @brief Ce fichier sert uniquement à réaliser un affichage correcte lors de l'éxecution
+ * @authro SD
+ */
+
 #ifndef TIMEOUT
 #define TIMEOUT 0
 #endif
@@ -17,20 +23,50 @@
 
 extern bool debug;
 
+/**
+ * @brief Permet d'afficher TIMEOUT en rouge
+ */
 void print_timeout();
 
+/**
+ * @brief Permet d'afficher SUCCESS en vert
+ */
 void print_success();
 
+/**
+ * @brief Permet d'afficher FAILED en rouge
+ */
 void print_fail();
 
+/**
+ * @brief Permet d'afficher DETECTION en orange
+ */
 void print_detection();
 
+/**
+ * @brief Permet d'afficher timeout/success/fail en fonction du retour du
+ * protocole de communication
+ * @param asserv_back: un entier correspondant au retour du prototocole, 1 pour
+ * timeout, 2 pour détection, 3 pour problème majeur et 0 en cas normal
+ */
 void affichage(int asserv_back);
 
+/**
+ * @brief Permet de savoir si le point actuel du robot est le point prévu
+ * @param result_x La coordonnée x du robot
+ * @param result_y La coordonnée y du robot
+ * @param dest_x La coordonnée x à comparer avec result_x
+ * @param dest_y La coordonnée y à comparer avec result_y */
 void good_port(short result_x, short result_y, short dest_x, short dest_y);
 
+/**
+ * @brief Permet d'afficher UNOPTIMIZED en orange*/
 void print_optimisated();
 
+/**
+ * @brief Permet d'afficher un résumé d'une éxecution. Permet d'afficher le nombre de succès, de timeout, d'échec et de fois où le robot
+ * est arrivé au point attendu. Si la variable debug est à 1 cela affiche aussi
+ * le nombre de trajectoires non optimisée*/
 void print_summarise();
 
 #endif // __AFFICHAGE_H_

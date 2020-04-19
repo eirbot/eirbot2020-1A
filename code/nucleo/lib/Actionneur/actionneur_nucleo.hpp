0bdfd8511562d627dd7e14e53d25d912e1314105
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-04-19 17:26:16 +0200

    Documentation : fin de la documentation

--- code/nucleo/lib/Actionneur/actionneur_nucleo.hpp
+++ code/nucleo/lib/Actionneur/actionneur_nucleo.hpp
@@ -6,8 +6,19 @@
 #define SERVO_LEFT 1900
 #define SERVO_RIGHT 6550
 
+/**
+ * @file actionneur_nucleo.hpp
+ * @brief Contient les fonctions permettant d'activer ou désactiver les actionneurs
+ * @author*/
+
+/**
+ * @brief Sort le pavillon
+ * @param servo: Le servomoteur associé au pavillon*/
 void activate_pavillon(PwmOut servo); //Met le pavillon dans l'autre configuration
 
+/**
+ * @brief Sort ou rentre le pavillon
+ * @param serv: Le servomoteur associé au manche*/
 void activate_manche(PwmOut serv); //Met le bras pour le manche dans l'autre configuration
 
 
--- code/nucleo/lib/Protocole/Protocole_nucleo.hpp
+++ code/nucleo/lib/Protocole/Protocole_nucleo.hpp
@@ -3,13 +3,22 @@
 
 #include "mbed.h"
 
+/**
+ * @file Protocole_nucleo.hpp
+ * @brief Fichier principal du projet permet de communiquer entre la raspberry et la nucléo. Ce fichier est l'implémentation côté nucléo
+ * @author Ptit Lu*/
+
+/**
+ * @class Protocole
+ * @brief Contient toutes les méthodes permettant d'utiliser le protocole côté nucléo*/
 class Protocole
 {
-    public:
+        public:
         Protocole();
         ~Protocole();
-
-        short x, y, angle;
+        short x;
+        short y;
+        short angle;
         char GP2_on;
         char GP2_etats[3];
         char actionneur_id;

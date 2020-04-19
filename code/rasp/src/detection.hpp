7a388686653a05720198e725508937f7df2d9c8d
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-04-19 17:04:42 +0200

    Documentation : Ajout de la documentation du dossier rasp/

--- code/rasp/src/detection.hpp
+++ code/rasp/src/detection.hpp
@@ -2,6 +2,9 @@
 #define __DETECTION_H__
 
 
+/**
+ * @file detection.hpp
+ * @brief Fichier permettant d'interfacer avec les GP2*/
 #include <stdio.h>
 #include <math.h>
 #include "world.hpp"
@@ -13,6 +16,17 @@
 // 0.72 -> 20 cm
 // O.19 -> 50 cm
 
+
+/**
+ * @enum GP2_name
+ * @brief Contient les noms des 6 GP2
+ * @param gauche_avant: GP2 gauche à l'avant
+ * @param centre_avant: GP2 central à l'avant
+ * @param droite_avant: GP2 droite à l'avant
+ * @param gauche_arriere: GP2 gauche à l'arrière
+ * @param centre_arriere: GP2 central à l'arrière
+ * @param droite_arriere: GP2 droite à l'arrière
+ */
 enum GP2_name {
   gauche_avant,
   centre_avant,
@@ -22,24 +36,61 @@ enum GP2_name {
   droite_arriere,
 };
 
+/**
+* @struct GP2_information
+* @brief J'ai oublié
+* @param input: le nom d'un GP2
+* @param mask: le mask appliqué à ce GP2
+ */
 struct GP2_information{
     enum GP2_name input;
     short mask; //Valeur entre 1 et 4
 };
 
+/**
+* @class GP2
+* @brief Contient les différentes méthodes pour initialiser, activer et désactiver les GP2 et détecter les obstacles
+ */
+
 class GP2{
 
     public:
-                GP2();
-                GP2(enum GP2_name input,int activated,int distance_seuil);
-                static void activate(); //Active la detection
-                static void disactivate(); //Désactive la detection
-                static vector<obstacle> gp2Obstacle(std::vector<obstacle> list_obstacles, struct position position); //Creer une interuption si les GP2 activés détectent quelque chose
-                ~GP2();
-    private:
-        enum GP2_name input; //identifiant du GP2
-        int activated; //0 si désactivé 1 sinon
-        int distance_seuil;
+    GP2();
+    /**
+    * @brief Initialise un GP2
+    * @param input: le nom du GP2
+    * @param activated: l'état du GP2, 1 activé, 0 désactivé
+    * @param distance_seuil: paramètre la distance à laquelle le GP2 doit détecter
+     */
+    GP2(enum GP2_name input,int activated,int distance_seuil);
+    /**
+    * @brief Active la détection
+     */
+    static void activate(); //Active la detection
+    /**
+    * @brief Désactive la détection
+     */
+    static void disactivate(); //Désactive la detection
+    /**
+    * @brief Une intéruption qui s'active lorsque l'un des GP2 activé détecte quelque chose
+    * @param[out] list_obstacles: La liste des obstacles en cours, la fonction va la modifier pour ajouter un obstacle si besoin
+    * @return Une liste d'obstacle actualisée avec les obstacles que les GP2 détectent
+     */
+    static vector<obstacle> gp2Obstacle(std::vector<obstacle> list_obstacles, struct position position); //Creer une interuption si les GP2 activés détectent quelque chose
+    ~GP2();
+  private:
+    /**
+    * @brief Identifiant du GP2
+     */
+    enum GP2_name input; //identifiant du GP2
+    /**
+    * @brief Entier correspondant à l'activation
+     */
+    int activated; //0 si désactivé 1 sinon
+    /**
+    * @brief Entier correspondant à la distance de détection
+     */
+    int distance_seuil;
 };
 
 #endif // __DETECTION_H__
--- code/rasp/src/world.hpp
+++ code/rasp/src/world.hpp
@@ -4,11 +4,33 @@
 #include <vector>
 using namespace std;
 
+/**
+ * @brief La largeur du robot*/
 #define ROBOT_WIDTH 32
+/**
+ * @brief La longueur du robot*/
 #define ROBOT_LENGTH 32
-#define X_MAX 300 //Longueur de la table
-#define Y_MAX 200 //Largeur de la table
+/**
+ * @brief La longueur de la table*/
+#define X_MAX 300
+/**
+ * @brief La largeur de la table*/
+#define Y_MAX 200
 
+/**
+ * @file world.hpp
+ * @brief Permet definir la table de jeu et les différentes structures nécessaires au fonctionnement de la navigation
+ * @author SD*/
+
+/**
+ * @brief Structure permettant de définir un Node, essentiel pour réaliser l'Astar
+ * @param x: coordonnée x
+ * @param y: coordonnée y
+ * @param parentX: coordonnée x du parent du point, rempli uniquement pendant l'exécution de l'Astar
+ * @param parentY: coordonnée y du parent du point, rempli uniquement pendant l'exécution de l'Astar
+ * @param gcost: cout entre le noeud de départ et le noeud actuel, non vide seulement si l'Astar est executé
+ * @param hcost: cout estimé entre le noeud actuel et le noeud d'arrivée, non vide seulement si l'Astar est executé
+ * @param fcost: plus petit cout parmis les voisins du noeud actuel, non vide seulement si l'Astar est executé*/
 struct Node{
         short x;
         short y;
@@ -20,11 +42,20 @@ struct Node{
 
 };
 
+/**
+ * @brief Permet de définir une forme pour ensuite être utilisé dans un obstacle
+ * @param width: la largeur de l'obstacle en centimètre
+ * @param length: la longeur de l'obstacle en centimètre*/
 struct shape{
     short width;
     short length;
 };
 
+/**
+ * @brief Permet de définir un obstacle sur la table
+ * @param center_x: la coordonnée x du centre de l'obstacle
+ * @param center_y: la coordonnée y du centre de l'obstacle
+ * @param shape: la forme de l'objet*/
 struct obstacle{
     short center_x;
     short center_y;
@@ -34,9 +65,19 @@ struct obstacle{
 
 class World{
     public:
+        /**
+         * @brief Permet de remplir un vecteur avec la liste des obstacles, c'est ici que l'on ajoute ou enlève des obstacles
+         * @return un vecteur avec la liste des obstacles*/
         static std::vector<obstacle> fillVector(); //Permet de remplir un vecteur avec la liste des obstacles
+        /**
+         * @brief Permet de savoir, si le robot est dans une position si il entre en collision avec un obstacle
+         * @param x: coordonnée x du point à tester
+         * @param y: coordonnée y du point à tester
+         * @param list_obstacles: listes des obstacles sur la table*/
         static bool isValid(short x, short y,std::vector<obstacle> list_obstacles); //Renvoie faux si la case n'est pas valide
     private:
+        /**
+         * @deprecated Ne sert plus à rien il me semble*/
         static bool who_here(short x, short y,struct shape shape, std::vector<obstacle> list_obstacles); //Permet de savoir si un obstacle rentre en conflit avec le robot
 };
 

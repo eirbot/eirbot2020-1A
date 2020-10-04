62cc9e6bc2ea5abe2c13861bd705e163eae2e41b
lucas <lucas-m3>
2020-10-04 19:27:19 +0200

    tentative position absolue
    a tester

--- code/nucleo/lib/Protocole/Protocole_nucleo.cpp
+++ code/nucleo/lib/Protocole/Protocole_nucleo.cpp
@@ -6,7 +6,8 @@
 Protocole::Protocole() {
     _serial = new RawSerial(USBTX, USBRX);
     _serial->baud(115200);
-    _serial->attach(this, &Protocole::poll, Serial::RxIrq);
+    _serial->attach(callback(this, &Protocole::readByte), Serial::RxIrq);
+    Protocole::state = INIT;
     wait_us(100000);
 }
 
@@ -16,16 +17,39 @@ Protocole::~Protocole() {
 // ATTENTION fonction bloquante
 // TODO gerer l'overflow du buffer
 // le while risque de bloquer et faire foirer l'asserv...
-void Protocole::poll() {
-    while(_serial->readable()) {
-        readBuffer[buf_index] = _serial->getc();
-        if(readBuffer[buf_index] == '\n') {
-            readBuffer[buf_index+1] = 0;
-            buf_index = 0;
-            parse();
-            break;
+// void Protocole::poll() {
+//     while(_serial->readable()) {
+//         readBuffer[buf_index] = _serial->getc();
+//         if(readBuffer[buf_index] == '\n') {
+//             readBuffer[buf_index+1] = 0;
+//             buf_index = 0;
+//             parse();
+//             break;
+//         }
+//         buf_index++;
+//     }
+// }
+
+void Protocole::readByte() {
+    readBuffer[buf_index] = _serial->getc();
+    buf_index++;
+    if(readBuffer[buf_index] == '\n') {
+        readBuffer[buf_index+1] = 0;
+        order_ready_flag = true;
+    }
+}
+
+void Protocole::update_state() {
+    if(order_ready_flag == true) {
+        buf_index = 0;
+        parse();
+    }
+}
+
+void Protocole::act() {
+    if(state == WAIT_ASSERV) {
+        if(get_feedback() == 1) {
         }
-        buf_index++;
     }
 }
 
@@ -33,9 +57,13 @@ void Protocole::poll() {
 void Protocole::parse() {
     //SET
     if(sscanf(readBuffer, "SPO%hd,%hd\n", &x, &y)) {
+        go_XY(x/100, y/100); //conversion cm -> m
+        state = WAIT_ASSERV;
         _serial->printf("RPOOK\n");
     }
     else if(sscanf(readBuffer, "SRO%hd\n", &angle)) {
+        rotate(angle);
+        state = WAIT_ASSERV;
         _serial->printf("RROOK\n");
     }
     else if(sscanf(readBuffer, "SGA%c\n", &GP2_on)) {

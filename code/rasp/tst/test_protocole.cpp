76bdbaafa14c5cd78b2b7a8564560e99ece0813f
lucas <lucas-m3>
2020-10-14 09:48:35 +0200

    debug protocole rasp

--- code/rasp/tst/test_protocole.cpp
+++ code/rasp/tst/test_protocole.cpp
@@ -1,7 +1,7 @@
 #include "test_protocole.hpp"
 #include <unistd.h>
 
-class Protocole proto("/dev/ttyACM0");
+// class Protocole proto("/dev/ttyACM0");
 
 void message_retour(Protocole::Etat etat) {
         switch(etat) {
@@ -26,43 +26,43 @@ void test_protocole(int argc, char *argv[]) {
         Protocole::Etat ret;
         usleep(100000); //0.1s
 
-        printf("Set GP2 on ..................... ");
-        ret = proto.set_detection_GP2(1);
-        message_retour(ret);
+        // printf("Set GP2 on ..................... ");
+        // ret = proto.set_detection_GP2(1);
+        // message_retour(ret);
 
         printf("Set Position ................... ");
-        ret = proto.set_position(40, 60, etats_GP2, 10);
+        ret = Protocole.set_position(50, 80, etats_GP2, 10);
         message_retour(ret);
 
-        printf("Set angle ...................... ");
-        ret = proto.set_angle(90);
-        message_retour(ret);
+        // printf("Set angle ...................... ");
+        // ret = proto.set_angle(90);
+        // message_retour(ret);
 
-        printf("Set actionneur ................. ");
-        ret = proto.set_actionneur('1', '1');
-        message_retour(ret);
+        // printf("Set actionneur ................. ");
+        // ret = proto.set_actionneur('1', '1');
+        // message_retour(ret);
 
-        struct position pos;
-        pos.x = 0;
-        pos.y = 0;
-        printf("Get Position ................... ");
-        ret = proto.get_position(&pos);
-        message_retour(ret);
-        printf("POS = %d, %d\n", pos.x, pos.y);
+        // struct position pos;
+        // pos.x = 0;
+        // pos.y = 0;
+        // printf("Get Position ................... ");
+        // ret = proto.get_position(&pos);
+        // message_retour(ret);
+        // printf("POS = %d, %d\n", pos.x, pos.y);
 
-        printf("Set Position ................... ");
-        ret = proto.set_position(60, 60, etats_GP2, 10);
-        message_retour(ret);
+        // printf("Set Position ................... ");
+        // ret = proto.set_position(60, 60, etats_GP2, 10);
+        // message_retour(ret);
 
-        short int angle = 666;
-        printf("Get angle ...................... ");
-        ret = proto.get_angle(&angle);
-        message_retour(ret);
-        printf("ANGLE = %hd\n", angle);
+        // short int angle = 666;
+        // printf("Get angle ...................... ");
+        // ret = proto.get_angle(&angle);
+        // message_retour(ret);
+        // printf("ANGLE = %hd\n", angle);
 
-        printf("Get etats ...................... ");
-        ret = proto.get_etats_GP2(etats_GP2);
-        message_retour(ret);
-        printf("Etats GP2 = %c, %c, %c\n", etats_GP2[0], etats_GP2[1], etats_GP2[2]);
+        // printf("Get etats ...................... ");
+        // ret = proto.get_etats_GP2(etats_GP2);
+        // message_retour(ret);
+        // printf("Etats GP2 = %c, %c, %c\n", etats_GP2[0], etats_GP2[1], etats_GP2[2]);
         TEST_END;
 }

d1b6144cba8cd37d5b71f5e7c9b042b91bcd1873
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-10-17 18:37:33 +0200

    miaou

--- code/rasp/src/main.cpp
+++ code/rasp/src/main.cpp
@@ -15,8 +15,8 @@ int total_optimized;
 int unoptimized;
 int total_deplacement;
 int success_deplacement;
-int total_reach;
 int success_reach;
+int total_reach;
 int timeout;
 int robot_adv;
 int timeout_after_timeout;
@@ -43,21 +43,6 @@ struct Node Port_S={20,150,0,0,0,0,0};
 // Définition des GP2
 
 
-void port_now(vector<obstacle> list_obstacles)
-{
-  printf("\033[33mJe pars du MANCHE_2 et je vais au PORT \033[0m \n");
-  std::vector<Node> result;
-  int boussole=rand() % 1;
-  struct position my_position=Robot.position();
-  struct Node position={(short) 63,(short) 184,0,0,0,0,0};
-  if (boussole==0) {
-    Robot.move(position,Port_N,list_obstacles);
-  }
-  else if(boussole==1){
-    Robot.move(position,Port_S,list_obstacles);
-  }
-}
-
 //Initialisation
 void setup()
 {
@@ -73,7 +58,7 @@ void setup()
 
   //Mise du robot au point de départ
   printf("Je me déplace jusqu'au point de départ \n");
-  go_to({.x=Port.x,.y=Port.y},robot_position());
+  go_to({.x=Port.x,.y=Port.y});
   printf("Déplacement au point de départ");
   print_success();
 }
@@ -87,84 +72,92 @@ void loop_blue()
   total_reach=4;
   while ((clock()-begin_time)/CLOCKS_PER_SEC <= 90) {
 
-    auto pos=Robot.position();
-
-    Node pos_node={.x= (short) pos.x,.y= (short) pos.y,0,0,0,0,0};
-    printf("%d %d \n", pos.x, pos.y);
-    go_to({.x=20,.y=80},pos);
-    Robot.move(pos_node, {.x=(short) 20,.y= (short) 80,0,0,0,0,0},list_obstacles);
+    go_to({.x=20,.y=80});
 
     //Module Phare
     printf("\033[33mJe pars du PORT et je vais au WAYPOINT \033[0m \n");
+    Node pos_node={.x= (short) 20,.y= (short) 80, 0,0,0,0,0};
     Robot.move(pos_node,Phare_blue_waypoint,list_obstacles);
 
     printf("\033[33mJe pars du WAYPOINT et je vais au PHARE \033[0m \n");
     Robot.move(Phare_blue_waypoint,Phare_blue,list_obstacles);
     Robot.actionneur(0,1);
-    pos=Robot.position();
-    go_to({.x=40,.y=20},pos);
+    go_to({.x=40,.y=20});
     Robot.actionneur(0, 0);
+    if (!Robot.communication()) {
+      printf("\033[34mLe phare n'a pas été activé, je réessaye \033[0m \n");
+      Robot.actionneur(1, 1);
+      go_to({.x=20,.y=20});
+      Robot.actionneur(0, 1);
+      go_to({.x=40,.y=20});
+    }
 
     //Module Manche à air
-
+    pos_node={.x= (short) 40,.y= (short) 20, 0,0,0,0,0};
     printf("\033[33mJe pars de PHARE et je vais à MANCHE_1 \033[0m \n");
     Robot.move(pos_node,Manche_1_blue,list_obstacles);
     Robot.actionneur(1, 1);
     printf("\033[33mJe pars de MANCHE_1 et je vais au MANCHE_2 \033[0m \n");
-    pos=Robot.position();
-    go_to({.x=53,.y=180},pos);
+    go_to({.x=53,.y=180});
     Robot.actionneur(1, 0);
-
-    //Lecture de la boussole
-    printf("\033[33mJe récupère l'information de la boussole \033[0m \n");
-
     break;
   }
+  //Lecture de la boussole
+  printf("\033[33mJe récupère l'information de la boussole \033[0m \n");
+  int boussole=Robot.communication();
   vector<obstacle> list_obstacles_no_ecocup = fillVector_no_ecocup();
-  port_now(list_obstacles_no_ecocup);
-}
 
-//Boucle de jeu
-void loop_yellow()
-{
-  Navigation Navigation;
-  int temps=0;
-  vector<obstacle> list_obstacles = fillVector();
-  total_reach=4;
-  while ((clock()-begin_time)/CLOCKS_PER_SEC <= 90) {
-
-    auto pos=Robot.position();
-    pos.x=16;
-    pos.y=80;
-    Node pos_node={.x= (short) pos.x,.y= (short) pos.y,0,0,0,0,0};
-    //Module Phare
-    printf("\033[33mJe pars du PORT et je vais au PHARE \033[0m \n");
-    Robot.move(pos_node,Phare_yellow,list_obstacles);
-    go_to({.x=pos.x-1, .y=pos.y-1}, pos);
-    Robot.rotation(-90);
-    Robot.actionneur(1,1);
-    pos=Robot.position();
-    go_to({.x=pos.x+20,.y=pos.y},pos);
-    Robot.actionneur(1, 0);
-
-    //Module Manche à air
-
-    printf("\033[33mJe pars de PHARE et je vais à MANCHE_1 \033[0m \n");
-    Robot.move(pos_node,Manche_1_yellow,list_obstacles);
-    Robot.rotation(90);
-    Robot.actionneur(0, 1);
-    printf("\033[33mJe pars de MANCHE_1 et je vais au MANCHE_2 \033[0m \n");
-    pos=Robot.position();
-    go_to({.x=pos.x+50,.y=pos.y},pos);
-    Robot.actionneur(0, 0);
-
-    //Lecture de la boussole
-    printf("\033[33mJe récupère l'information de la boussole \033[0m \n");
-
-    break;
+  printf("\033[33mJe pars du MANCHE_2 et je vais au PORT %d \033[0m \n",boussole);
+  Node position={(short) 63,(short) 184,0,0,0,0,0};
+  if (!Robot.communication()) {
+    Robot.move(position,Port_N,list_obstacles_no_ecocup);
   }
-  port_now(list_obstacles);
+  else if(Robot.communication()){
+    Robot.move(position,Port_S,list_obstacles_no_ecocup);
+  }
 }
+
+// //Boucle de jeu
+// void loop_yellow()
+// {
+//   Navigation Navigation;
+//   int temps=0;
+//   vector<obstacle> list_obstacles = fillVector();
+//   total_reach=4;
+//   while ((clock()-begin_time)/CLOCKS_PER_SEC <= 90) {
+
+//     auto pos=Robot.position();
+//     pos.x=16;
+//     pos.y=80;
+//     Node pos_node={.x= (short) pos.x,.y= (short) pos.y,0,0,0,0,0};
+//     //Module Phare
+//     printf("\033[33mJe pars du PORT et je vais au PHARE \033[0m \n");
+//     Robot.move(pos_node,Phare_yellow,list_obstacles);
+//     go_to({.x=pos.x-1, .y=pos.y-1}, pos);
+//     Robot.rotation(-90);
+//     Robot.actionneur(1,1);
+//     pos=Robot.position();
+//     go_to({.x=pos.x+20,.y=pos.y},pos);
+//     Robot.actionneur(1, 0);
+
+//     //Module Manche à air
+
+//     printf("\033[33mJe pars de PHARE et je vais à MANCHE_1 \033[0m \n");
+//     Robot.move(pos_node,Manche_1_yellow,list_obstacles);
+//     Robot.rotation(90);
+//     Robot.actionneur(0, 1);
+//     printf("\033[33mJe pars de MANCHE_1 et je vais au MANCHE_2 \033[0m \n");
+//     pos=Robot.position();
+//     go_to({.x=pos.x+50,.y=pos.y},pos);
+//     Robot.actionneur(0, 0);
+
+//     //Lecture de la boussole
+//     printf("\033[33mJe récupère l'information de la boussole \033[0m \n");
+
+//     break;
+//   }
+//   port_now(list_obstacles);
+// }
 
 
 class Protocole Protocole("/dev/ttyACM0");
@@ -175,10 +168,10 @@ int main(int argc, char *argv[]) {
   if (side=="blue") {
     loop_blue();
   }
-  else if(side=="yellow") {
-    loop_yellow();
-  }
+  // else if(side=="yellow") {
+  //   loop_yellow();
+  // }
   delai=(clock()-begin_time)/CLOCKS_PER_SEC;
-  printf("Temps d'execution %f secondes \n",delai);
+  printf("Temps d'execution \033[32m%f\033[0m secondes \n",delai);
   return 0;
 }

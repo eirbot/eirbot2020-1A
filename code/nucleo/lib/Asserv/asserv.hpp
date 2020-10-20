2a5fc566b44b30a04d20185b0f9bc44e810781d4
lucas <lucas-m3>
2020-10-20 20:12:28 +0200

    limit acceleration

--- code/nucleo/lib/Asserv/asserv.hpp
+++ code/nucleo/lib/Asserv/asserv.hpp
@@ -7,7 +7,7 @@
 #include "asserv_vitesse_angle.hpp"
 #include "math.h"
 
-#define A_MAX 0.0002
+#define A_MAX 10
 
 // TODO
 // - goto_XY
@@ -24,6 +24,7 @@ enum asserv_state {PO_ANGLE, PO_DISTANCE, PO_STOP, ROT, STOP, RES};
 void init_asserv(void);
 void reset_consigne(void); //update derniere position connue
 void set_pwm(void);
+int sign(float x);
 void function_Asserv(void); //THE function
 void update_state(void);
 void set_state(enum asserv_state s);
@@ -31,7 +32,7 @@ void set_state_distance(void);
 enum asserv_state get_state(void);
 void set_consigne(char c);
 void print_debug_asserv(Serial &pc,char c);
-char * update_debug_string();
+char *update_debug_string();
 
 float XY_to_Distance(float x, float y);
 float XY_to_Angle(float x, float y);

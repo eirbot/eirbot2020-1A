2fb7858cc70308bcf865ccb3769805eceb148a72
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2020-10-23 08:28:51 +0200

    add test low level

---
+++ code/rasp/tst/test_lowlevel.hpp
@@ -0,0 +1,10 @@
+#ifndef __TEST_LOWLEVEL_H_
+#define __TEST_LOWLEVEL_H_
+
+#include "test.hpp"
+#include "../src/interactions/low_level.hpp"
+
+void test_lowlevel();
+
+
+#endif // __TEST_LOWLEVEL_H_
